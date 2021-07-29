2cf9be538e213575efaf943884f7af16885151b0
Eya <eboundka@gmail.com>
2021-07-29 01:29:43 +0100

    user managment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ommek-sannefa'`, () => {
+  it(`should have as title 'Ommek Sannefa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ommek-sannefa');
+    expect(app.title).toEqual('Ommek Sannefa');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ommek-sannefa app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Ommek Sannefa app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ommek-sannefa';
+  title = 'Ommek Sannefa';
+  
 }
